73913dcd84cdce56aefbac07c2e182f71d68f1bb
Андрій Мірошник <mirik23452@gmail.com>
2023-09-15 00:26:34 +0300

    Better renderTexture

--- Grenadier/Assets/Scripts/Target.cs
+++ Grenadier/Assets/Scripts/Target.cs
@@ -6,67 +6,73 @@ using Vector3 = System.Numerics.Vector3;
 
 public class Target : MonoBehaviour
 {
-    [SerializeField] private RenderTexture renderTexture; // renderTextuer that you will be rendering stuff on
-    [SerializeField] private Renderer renderer; // renderer in which you will apply changed texture
-    [SerializeField] private Texture2D stampTexture; //Texture to Graphics.Drawtexture on my RenderTexture.
+    [SerializeField] private RenderTexture renderTexture;
+    [SerializeField] private Renderer renderer;
+    [SerializeField] private Texture2D stampTexture;
 
     [SerializeField] private Vector2Int position = new Vector2Int(1024, 1024);
     [SerializeField] private Color backColor = Color.white;
+    private List<Vector2Int> _points = new List<Vector2Int>();
     private Texture2D _texture;
     private int GetStampHalfSize => stampTexture.width / 2;
-    private List<Vector2Int> _points=new List<Vector2Int>();
-    private bool _updated;
 
     private void Start()
     {
         _texture = new Texture2D(renderTexture.width, renderTexture.height);
         renderer.material.mainTexture = _texture;
+        ResetTexture();
 
         RenderTexture.active = renderTexture;
+
+        GL.Clear(true, true, backColor);
+        ResetTexture();
+        RenderTexture.active = null;
+    }
+
+    private void ResetTexture()
+    {
+        Color[] pixels = new Color[renderTexture.width * renderTexture.height];
+
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = backColor;
+        }
+
+        _texture.SetPixels(pixels);
+        _texture.Apply();
     }
 
     public void AddPoint(Vector2 vector2)
     {
-        Debug.Log(vector2);
-
-        Vector2Int intVector = new Vector2Int((int)(vector2.x*renderTexture.width), (int)(vector2.y*renderTexture.width));
+        Vector2Int intVector =
+            new Vector2Int((int)(vector2.x * renderTexture.width), (int)(vector2.y * renderTexture.width));
         _points.Add(intVector);
-        _updated = true;
-    }
-
-    private void Update()
-    {
-        if (_updated)
-        {
-            Rerender();
-            _updated = false;
-        }
+        Rerender();
     }
 
     private void Rerender()
     {
-        RenderTexture.active = renderTexture; 
+        RenderTexture.active = renderTexture;
 
-        _texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
-        for (int i = 0; i < renderTexture.width; i++)
-        for (int j = 0; j < renderTexture.height; j++)
+        _texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0, false);
+        foreach (var point in _points)
         {
-            Color pointColor = backColor;
-            foreach (var point in _points)
+            Color[] stampColors = new Color[stampTexture.width * stampTexture.height];
+
+            for (int i = 0; i < stampTexture.width; i++)
+            for (int j = 0; j < stampTexture.height; j++)
             {
-                if (i> point.x - GetStampHalfSize && i < point.x + GetStampHalfSize 
-                 && j > point.y - GetStampHalfSize && j < point.y + GetStampHalfSize)
-                {
-
-                    float t = stampTexture.GetPixel(i-(point.x - GetStampHalfSize), j-(point.y - GetStampHalfSize)).a;
-                    pointColor = (1 - t) * pointColor;
-                    
-                }
+                int x = point.x - GetStampHalfSize + i;
+                int y = point.y - GetStampHalfSize + j;
+                float t = stampTexture.GetPixel(i, j).a;
+                stampColors[j * stampTexture.width + i] = (1 - t) * _texture.GetPixel(x,y);
             }
-            _texture.SetPixel(i, j,pointColor);
 
+            _texture.SetPixels(point.x - GetStampHalfSize, point.y - GetStampHalfSize, stampTexture.width,
+                stampTexture.height, stampColors);
         }
+
         _texture.Apply();
-        RenderTexture.active = null; //don't forget to set it back to null once you finished playing with it. 
+        RenderTexture.active = null;
     }
 }
